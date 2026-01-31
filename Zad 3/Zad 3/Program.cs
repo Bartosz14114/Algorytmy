4640227c099b4112622816690c8684e71bee6da1
Bartosz14114 <bcelmer01@gmail.com>
2026-01-31 01:35:10 +0100

    Add files via upload

---
+++ Zad 3/Zad 3/Program.cs
@@ -0,0 +1,14 @@
+﻿public static (int, int) WskażPowtórkę(double[] liczby)
+{
+    for (int i = 0; i <= liczby.Length - 3; i++)
+    {
+        for (int j = i + 1; j <= liczby.Length - 3; j++)
+        {
+            if (liczby[i] + liczby[i + 1] + liczby[i + 2] == liczby[j] + liczby[j + 1] + liczby[j + 2])
+            {
+                return (i, j);
+            }
+        }
+    }
+    return (-1, -1);
+}

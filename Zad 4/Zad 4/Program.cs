9f61e8918512c98040fdd58ec541882f99f5bca2
Bartosz14114 <bcelmer01@gmail.com>
2026-01-31 01:35:26 +0100

    Add files via upload

---
+++ Zad 4/Zad 4/Program.cs
@@ -0,0 +1,38 @@
+﻿public static bool SprawdźNawiasy(string napis)
+{
+    Stack<char> stos = new Stack<char>();
+    var pary = new Dictionary<char, char>
+    {
+        { '”', '„' },
+        { '«', '»' },
+        { ')', '(' }
+    };
+
+    foreach (char c in napis)
+    {
+        if (c == '„' || c == '»' || c == '(')
+        {
+            stos.Push(c);
+        }
+        else if (pary.ContainsKey(c))
+        {
+            if (stos.Count == 0 || stos.Pop() != pary[c])
+            {
+                Console.WriteLine("Błąd: Niepoprawne domknięcie bloku.");
+                return false;
+            }
+        }
+    }
+
+    if (stos.Count > 0)
+    {
+        Console.WriteLine("Błąd: Pozostały niedomknięte bloki:");
+        foreach (char s in stos)
+        {
+            Console.WriteLine(s);
+        }
+        return false;
+    }
+
+    return true;
+}
